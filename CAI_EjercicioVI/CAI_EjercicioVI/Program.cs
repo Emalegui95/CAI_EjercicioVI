490c935ac5de8ab7a9158bbaf6a0b8f29d879eaa
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-03-23 20:03:22 -0300

    Ejercicio VI

---
+++ CAI_EjercicioVI/CAI_EjercicioVI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioVI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro número: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El resultado de la cuenta es: " + ((a+b)*(a-b)));
+            Console.ReadKey();
+        }
+    }
+}
